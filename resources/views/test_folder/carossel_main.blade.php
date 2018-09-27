bfd7c26bd8bc146715914cc52731ef1f9acae6dd
Creig Fortune <fortunephiri004@gmail.com>
2018-09-27 14:01:18 -0400

    change to test layout. using bootstrap 4.1. pages css within the current page. area for the side and post area greyed out. trending now not available in a small display. whole div falls away

--- resources/views/test_folder/carossel_main.blade.php
+++ resources/views/test_folder/carossel_main.blade.php
@@ -111,84 +111,58 @@
 
 
 
-
-
     <!-- details card section starts from here -->
 <div class="container  ">
     <h2 class="text-center mb-3">Trending now</h2>
-
-
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="card-content">
-                            <div class="card-img">
-                                <img src="https://placeimg.com/380/230/nature" alt="">
-                                <span><h4>#bringBack</h4></span>
-                            </div>
-                            <div class="card-desc">
-                                <h3>Dear Mr President</h3>
-                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, voluptatum! Dolor quo, perspiciatis
-                                    voluptas totam</p>
-                                <a href="#" class="btn-card">Read</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="card-content">
-                            <div class="card-img">
-                                <img src="https://placeimg.com/380/230/animals" alt="">
-                                <span><h4>heading2</h4></span>
-                            </div>
-                            <div class="card-desc">
-                                <h3>Bring back Zimbabwe</h3>
-                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, voluptatum! Dolor quo, perspiciatis
-                                    voluptas totam</p>
-                                <a href="#" class="btn-card">Read</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="card-content">
-                            <div class="card-img">
-                                <img src="https://placeimg.com/380/230/tech" alt="">
-                                <span><h4>heading3</h4></span>
-                            </div>
-                            <div class="card-desc">
-                                <h3>Header 3</h3>
-                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, voluptatum! Dolor quo, perspiciatis
-                                    voluptas totam</p>
-                                <a href="#" class="btn-card">Read</a>
-                            </div>
-                        </div>
-                    </div>
+<div class="d-sm-none d-lg-block">
+    <div class="row">
+        <div class="col-md-4 ">
+            <div class="card-content">
+                <div class="card-img">
+                    <img src="https://placeimg.com/380/230/nature" alt="">
+                    <span><h4>#bringBack</h4></span>
+                </div>
+                <div class="card-desc">
+                    <h3>Dear Mr President</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, voluptatum! Dolor quo, perspiciatis
+                        voluptas totam</p>
+                    <a href="#" class="btn-card">Read</a>
                 </div>
             </div>
-            <div class="carousel-item">
-                <p>this is a div</p>
-                <img class="d-block " src="..." alt="Second slide">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block" src="..." alt="Third slide">
+        </div>
+        <div class="col-md-4">
+            <div class="card-content">
+                <div class="card-img">
+                    <img src="https://placeimg.com/380/230/animals" alt="">
+                    <span><h4>heading2</h4></span>
+                </div>
+                <div class="card-desc">
+                    <h3>Bring back Zimbabwe</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, voluptatum! Dolor quo, perspiciatis
+                        voluptas totam</p>
+                    <a href="#" class="btn-card">Read</a>
+                </div>
             </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
+        <div class="col-md-4 ">
+            <div class="card-content">
+                <div class="card-img">
+                    <img src="https://placeimg.com/380/230/tech" alt="">
+                    <span><h4>heading3</h4></span>
+                </div>
+                <div class="card-desc">
+                    <h3>Header 3</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, voluptatum! Dolor quo, perspiciatis
+                        voluptas totam</p>
+                    <a href="#" class="btn-card">Read</a>
+                </div>
+            </div>
+        </div>
     </div>
-
-
-
 </div>
+
+    <div class="row mt-3">
+        <div class="col-4 bg-dark p-5">side bar</div>
+        <div class="col-8 bg-warning p-5">posts area</div>
+    </div>
+    </div>
