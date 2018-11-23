bf2d58e10cb05e6fc3d36cf748cfc87f9f90d73f
Creig Fortune <fortunephiri004@gmail.com>
2018-11-22 21:34:54 -0500

    changing the comment area to something more personal. 'whats on your mind ,{{name}}' from 'type here'

--- resources/views/test_folder/carossel_main.blade.php
+++ resources/views/test_folder/carossel_main.blade.php
@@ -24,20 +24,20 @@
                 <div class="image_outer_container">
                     <div class="green_icon text-center"></div>
                     <div class="image_inner_container">
-                        <img src="https://i0.wp.com/tricksmaze.com/wp-content/uploads/2017/04/Stylish-Girls-Profile-Pictures-36.jpg?resize=300%2C300&ssl=1">
+                        <img src="https://gdb.voanews.com/D23422B2-F20A-4202-9DE8-6CB85AE16E6E_cx17_cy14_cw75_w1023_r1_s.jpg">
                     </div>
                     <div class="mt-3">
-                        <h3 class="text-center">{{'@'.Auth::user()->name}}</h3>
+                        <h3 class="text-center youTube-red">{{'@'.Auth::user()->name}}</h3>
                     </div>
                 </div>
             </div>
         </div>
           <div class="col">
-             <h2 class="text-center youTube-red">Timeline</h2>
+             <h2 class="ml-5 color-dd-gradient text-center">Timeline</h2>
              <div class="row">
                                 <form method="post" action="" class="col-8 mx-auto row">
                                     {{ csrf_field() }}
-                                <textarea class="col-12" required name="post_body" id="post_body" placeholder="type here"></textarea>
+                                <textarea class="col-12" required name="post_body" id="post_body" placeholder="What's on your mind, {{Auth::user()->name}}"></textarea>
                                 <input type="submit" class="form-control btn btn-outline-danger" style="">
                                 </form>
 
@@ -49,7 +49,7 @@
           </div>
 
         <div class="col-1">
-            <h4 class="text-center m-3 youTube-red">Archives</h4>
+            <h4 class="text-center m-3 color-dd-gradient">Archives</h4>
             <ul class="list-group list-group-flush">
                 @foreach($archives as $archive)
                     <li class="list-group-item">
