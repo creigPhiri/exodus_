c90f59880e573c2b4f90691cfaf72a4d5fd05f4b
Creig Fortune <fortunephiri004@gmail.com>
2019-01-23 17:20:25 -0500

    update to profile.blade.php image width now set 200px and height auto.

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -1,6 +1,6 @@
-<nav class="col-md-3 d-none d-sm-block sidebar mt-5">
-    <div class="mx-auto mb-3 border-danger" style="border: 1px solid black; height: 100px; width: 100px">
-       <img src={{URL::to('/').'/images/' . Auth::user()->image_url}} />
+<nav class="col-md-3 row d-none d-sm-block sidebar mt-5">
+    <div class="col-8 offset-1 mb-3 border-danger">
+       <img class="img-responsive" style="width: 200px; height: auto" src={{URL::to('/').'/images/' . Auth::user()->image_url}} />
     </div>
         <form action="/imageUpload" method="post" enctype="multipart/form-data">
             {{ csrf_field() }}
