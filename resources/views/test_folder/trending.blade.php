9d4f85574b610041b4aae508b01e3a7baa70321c
Creig Fortune <fortunephiri004@gmail.com>
2019-02-07 00:01:45 -0500

    update to trending card. changed the image to circular shape with the @username and time stamp on its side

--- resources/views/test_folder/trending.blade.php
+++ resources/views/test_folder/trending.blade.php
@@ -10,60 +10,125 @@
 @include('layouts.nav')
 
     <h2 class="text-center h2">Trending now</h2>
-<div class="row  justify-content-center">
+<div class="row ">
 
-        <div class="col-10 col-md-8">
+        <div class="col-sm-12 col-md-10 offset-md-2 mt-5" >
+
             <div class="card-content">
-                <div class="card-img mx-5 mt-1">
-                    <img src="https://placeimg.com/380/230/nature" alt="">
-                    <span><h4>#bringBack</h4></span>
-                </div>
+                <div class="card-header" style="background-color: white; border-bottom: none">
+                   <div class="row">
+                       <div class=" col-5 text-right">
+                           <img src="https://placeimg.com/380/230/nature" alt="" width="120px" height="120px" class="rounded-circle img-responsive">
+                       </div>
+                       <div class="col-7 my-auto pl-5">
+                           <h3 class="text-primary mb-3">@Zim_1</h3>
+                           <span class="text-muted">5 days ago</span>
+                       </div>
+                   </div>
+                   </div>
+
+
                 <div class="card-desc">
                     <h3>Dear Mr President</h3>
                       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, voluptatum! Dolor quo, perspiciatis
-                     voluptas totam</p>
+                     voluptas totam. this is a test of what happens when the text becomes too long. obviously i could have used ipsum loreum. but hey i am creig</p>
                     <a href="#" class="btn-card">Read</a>
+                    <a href="#" class="active float-right" >Comments (5)</a>
                 </div>
             </div>
+        {{--</div>--}}
+        {{--<div class="col-md-2">--}}
+            {{--<div class="card-content">--}}
+                {{--<div class="row mt-2 mb-2">--}}
+                {{--<div class=" card-img">--}}
+                   {{--<img class="img-responsive rounded mx-auto my-3" src="https://www.aljazeera.com/mritems/imagecache/mbdxxlarge/mritems/Images/2017/11/20/a901e05caa0649339ef3cda148ee1d3a_18.jpg" alt="">--}}
+                {{--</div>--}}
+                {{--<div class="card-desc">--}}
+                    {{--<p>gucci grace <i></i></p>--}}
+                    {{--<small class="text-muted">30days ago</small><a href="#" class="active">@i_are_omar</a>--}}
+                {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--<div class="col-md-2">--}}
+        {{--<div class="card-content">--}}
+            {{--<div class="row mt-2 mb-2">--}}
+                {{--<div class=" card-img">--}}
+                    {{--<img class="img-responsive rounded mx-auto my-3" src="https://harareblitz.com/wp-content/uploads/2018/09/Mnangagwa-sad_600x387-1-600x381.jpg" alt="">--}}
+                {{--</div>--}}
+                {{--<div class="card-desc">--}}
+                    {{--<p>Forgive us mr cocodile <i></i></p>--}}
+                    {{--<small class="text-muted">30days ago</small><a href="#" class="active">@i_are_omar</a>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+    {{--<div class="col-md-2">--}}
+        {{--<div class="card-content">--}}
+            {{--<div class="row mt-2 mb-2">--}}
+                {{--<div class=" card-img">--}}
+                    {{--<img class="img-responsive rounded mx-auto my-3" src="https://www.aljazeera.com/mritems/imagecache/mbdxxlarge/mritems/Images/2017/10/22/03cefdee531c450fa139a68533784c33_18.jpg" alt="">--}}
+                {{--</div>--}}
+                {{--<div class="card-desc">--}}
+                    {{--<p>Bob vs the crocodile<i></i></p>--}}
+                    {{--<small class="text-muted">30days ago</small><a href="#" class="active">@i_are_omar</a>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+    </div>
+    <div class="col-sm-12 col-md-10 offset-md-2 mt-5" >
+
+        <div class="card-content">
+            <div class="card-header" style="background-color: white; border-bottom: none">
+                <div class="row">
+                    <div class=" col-5 text-right">
+                        <img src="https://placeimg.com/380/230/nature" alt="" width="120px" height="120px" class="rounded-circle img-responsive">
+                    </div>
+                    <div class="col-7 my-auto pl-5">
+                        <h3 class="text-primary">@Zim_1</h3>
+                        <span class="text-muted">5 days ago</span>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="card-desc">
+                <h3>Dear Mr President</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, voluptatum! Dolor quo, perspiciatis
+                    voluptas totam. this is a test of what happens when the text becomes too long. obviously i could have used ipsum loreum. but hey i am creig</p>
+                <a href="#" class="btn-card">Read</a>
+                <a href="#" class="active float-right" >Comments (5)</a>
+            </div>
         </div>
-        <div class="col-md-2">
+    </div>
+
+
+        <div class="col-sm-12 col-md-10 offset-md-2 mt-5" >
+
             <div class="card-content">
-                <div class="row mt-2 mb-2">
-                <div class=" card-img">
-                   <img class="img-responsive rounded mx-auto my-3" src="https://www.aljazeera.com/mritems/imagecache/mbdxxlarge/mritems/Images/2017/11/20/a901e05caa0649339ef3cda148ee1d3a_18.jpg" alt="">
+                <div class="card-header" style="background-color: white; border-bottom: none">
+                    <div class="row">
+                        <div class=" col-5 text-right">
+                            <img src="https://placeimg.com/380/230/nature" alt="" width="120px" height="120px" class="rounded-circle img-responsive">
+                        </div>
+                        <div class="col-7 my-auto pl-5">
+                            <h3 class="text-primary">@Zim_1</h3>
+                            <span class="text-muted">5 days ago</span>
+                        </div>
+                    </div>
                 </div>
+
+
                 <div class="card-desc">
-                    <p>gucci grace <i></i></p>
-                    <small class="text-muted">30days ago</small><a href="#" class="active">@i_are_omar</a>
+                    <h3>Dear Mr President</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, voluptatum! Dolor quo, perspiciatis
+                        voluptas totam. this is a test of what happens when the text becomes too long. obviously i could have used ipsum loreum. but hey i am creig</p>
+                    <a href="#" class="btn-card">Read</a>
+                    <a href="#" class="active float-right" >Comments (5)</a>
                 </div>
-                </div>
-            </div>
-        </div>
-    <div class="col-md-2">
-        <div class="card-content">
-            <div class="row mt-2 mb-2">
-                <div class=" card-img">
-                    <img class="img-responsive rounded mx-auto my-3" src="https://harareblitz.com/wp-content/uploads/2018/09/Mnangagwa-sad_600x387-1-600x381.jpg" alt="">
-                </div>
-                <div class="card-desc">
-                    <p>Forgive us mr cocodile <i></i></p>
-                    <small class="text-muted">30days ago</small><a href="#" class="active">@i_are_omar</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-2">
-        <div class="card-content">
-            <div class="row mt-2 mb-2">
-                <div class=" card-img">
-                    <img class="img-responsive rounded mx-auto my-3" src="https://www.aljazeera.com/mritems/imagecache/mbdxxlarge/mritems/Images/2017/10/22/03cefdee531c450fa139a68533784c33_18.jpg" alt="">
-                </div>
-                <div class="card-desc">
-                    <p>Bob vs the crocodile<i></i></p>
-                    <small class="text-muted">30days ago</small><a href="#" class="active">@i_are_omar</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    </div>
+            </div></div>
+
 </div>
+</div>
+
