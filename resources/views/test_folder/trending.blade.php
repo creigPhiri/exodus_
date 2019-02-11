dcd3b873e21994550851130fa49db471fec8ffe2
Creig Fortune <fortunephiri004@gmail.com>
2019-02-11 17:13:33 -0500

    update to trending.blade. restructuring app flow. from facebook comment structure to diary entry. comments can only be made on trending content. currently trending.blade holds the card template

--- resources/views/test_folder/trending.blade.php
+++ resources/views/test_folder/trending.blade.php
@@ -3,132 +3,48 @@
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <link href="{{ asset('css/main.css') }}" rel="stylesheet">
-
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+      integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 
 
 <div class="container  ">
 @include('layouts.nav')
 
     <h2 class="text-center h2">Trending now</h2>
-<div class="row ">
+<div class="row " style="max-height: 100px ">
 
-        <div class="col-sm-12 col-md-12  mt-5" >
-
-            <div class="card-content">
+        <div class="col-sm-12 col-md-12  mt-5 col-lg-6 offset-lg-3 " >
+            <div class="card-content pt-3">
                 <div class="card-header" style="background-color: white; border-bottom: none">
                    <div class="row">
-                       <div class=" col-5 text-right">
+                       <div class=" col-auto offset-1 offset-lg-1 col-lg-4 ">
                            <img src="https://placeimg.com/380/230/nature" alt="" width="120px" height="120px" class="rounded-circle img-responsive">
                        </div>
-                       <div class="col-7 my-auto pl-5">
-                           <h3 class="text-primary mb-3">@Zim_1</h3>
-                           <span class="text-muted">5 days ago</span>
+                       <div class=" ml-5 my-auto">
+                           <h3 class="text-secondary card-title">Dear Mr President</h3>
+                            <span class="text-primary h4 mb-2 d-inline">@Zim_1</span>
+                            <span class="text-muted pl-5">5 days ago</span>
                        </div>
                    </div>
-                   </div>
-
-
-                <div class="card-desc">
-                    <h3>Dear Mr President</h3>
-                      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, voluptatum! Dolor quo, perspiciatis
-                     voluptas totam. this is a test of what happens when the text becomes too long. obviously i could have used ipsum loreum. but hey i am creig</p>
-                    <a href="#" class="btn-card">Read</a>
-                    <a href="#" class="active float-right" >Comments (5)</a>
                 </div>
-            </div>
-        {{--</div>--}}
-        {{--<div class="col-md-2">--}}
-            {{--<div class="card-content">--}}
-                {{--<div class="row mt-2 mb-2">--}}
-                {{--<div class=" card-img">--}}
-                   {{--<img class="img-responsive rounded mx-auto my-3" src="https://www.aljazeera.com/mritems/imagecache/mbdxxlarge/mritems/Images/2017/11/20/a901e05caa0649339ef3cda148ee1d3a_18.jpg" alt="">--}}
-                {{--</div>--}}
-                {{--<div class="card-desc">--}}
-                    {{--<p>gucci grace <i></i></p>--}}
-                    {{--<small class="text-muted">30days ago</small><a href="#" class="active">@i_are_omar</a>--}}
-                {{--</div>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--<div class="col-md-2">--}}
-        {{--<div class="card-content">--}}
-            {{--<div class="row mt-2 mb-2">--}}
-                {{--<div class=" card-img">--}}
-                    {{--<img class="img-responsive rounded mx-auto my-3" src="https://harareblitz.com/wp-content/uploads/2018/09/Mnangagwa-sad_600x387-1-600x381.jpg" alt="">--}}
-                {{--</div>--}}
-                {{--<div class="card-desc">--}}
-                    {{--<p>Forgive us mr cocodile <i></i></p>--}}
-                    {{--<small class="text-muted">30days ago</small><a href="#" class="active">@i_are_omar</a>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-    {{--<div class="col-md-2">--}}
-        {{--<div class="card-content">--}}
-            {{--<div class="row mt-2 mb-2">--}}
-                {{--<div class=" card-img">--}}
-                    {{--<img class="img-responsive rounded mx-auto my-3" src="https://www.aljazeera.com/mritems/imagecache/mbdxxlarge/mritems/Images/2017/10/22/03cefdee531c450fa139a68533784c33_18.jpg" alt="">--}}
-                {{--</div>--}}
-                {{--<div class="card-desc">--}}
-                    {{--<p>Bob vs the crocodile<i></i></p>--}}
-                    {{--<small class="text-muted">30days ago</small><a href="#" class="active">@i_are_omar</a>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-    </div>
-    <div class="col-sm-12 col-md-12 mt-5" >
+                <div class="card-desc col-10 offset-1 text-truncate">
+                    <p class="text-secondary">Do you hate Zimbabwe that much? Have the good people done you wrong? You are the first citizen. Is it not for you
+                        to protect the flock? How long shall my people cry. How long shall a minority hold an entire nation hostage.
+                        How long shall be the voice of the people be silenced. How long shall my people starve.
+                    </p>
+                    <p class="text-secondary ">Do you hate Zimbabwe that much? Have the good people done you wrong? You are the first citizen. Is it not for you
+                        to protect the flock? How long shall my people cry. How long shall a minority hold an entire nation hostage.
+                        How long shall be the voice of the people be silenced. How long shall my people starve.
+                    </p>
+                    <div class="text-right pr-5">
+                        <a href="#" class="card-link">Read</a>
+                        <a href="#" class="card-link" ><i class="far fa-comment fa-lg" ></i> 5</a>
+                        <a href="#" class="card-link"><i class="far fa-thumbs-up fa-lg"></i></a>
+                    </div>
 
-        <div class="card-content">
-            <div class="card-header" style="background-color: white; border-bottom: none">
-                <div class="row">
-                    <div class=" col-5 text-right">
-                        <img src="https://placeimg.com/380/230/nature" alt="" width="120px" height="120px" class="rounded-circle img-responsive">
-                    </div>
-                    <div class="col-7 my-auto pl-5">
-                        <h3 class="text-primary">@Zim_1</h3>
-                        <span class="text-muted">5 days ago</span>
-                    </div>
                 </div>
-            </div>
-
-
-            <div class="card-desc">
-                <h3>Dear Mr President</h3>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, voluptatum! Dolor quo, perspiciatis
-                    voluptas totam. this is a test of what happens when the text becomes too long. obviously i could have used ipsum loreum. but hey i am creig</p>
-                <a href="#" class="btn-card">Read</a>
-                <a href="#" class="active float-right" >Comments (5)</a>
-            </div>
+                <div class="pb-3"></div>
+             </div>
         </div>
-    </div>
-
-
-        <div class="col-sm-12 col-md-12  mt-5" >
-
-            <div class="card-content">
-                <div class="card-header" style="background-color: white; border-bottom: none">
-                    <div class="row">
-                        <div class=" col-5 text-right">
-                            <img src="https://placeimg.com/380/230/nature" alt="" width="120px" height="120px" class="rounded-circle img-responsive">
-                        </div>
-                        <div class="col-7 my-auto pl-5">
-                            <h3 class="text-primary">@Zim_1</h3>
-                            <span class="text-muted">5 days ago</span>
-                        </div>
-                    </div>
-                </div>
-
-
-                <div class="card-desc">
-                    <h3>Dear Mr President</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, voluptatum! Dolor quo, perspiciatis
-                        voluptas totam. this is a test of what happens when the text becomes too long. obviously i could have used ipsum loreum. but hey i am creig</p>
-                    <a href="#" class="btn-card">Read</a>
-                    <a href="#" class="active float-right" >Comments (5)</a>
-                </div>
-            </div></div>
-
 </div>
 </div>
-
