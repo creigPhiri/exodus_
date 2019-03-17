208aac708839f981d6b7f1e6f5aaedb47dd5e067
Creig Fortune <fortunephiri004@gmail.com>
2019-03-17 00:15:52 -0400

    update to master.blade changing the script reference to secure_asset for loading the css

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -19,8 +19,8 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/style.css') }}" rel="stylesheet">
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 
     <!-- font awesome-->
