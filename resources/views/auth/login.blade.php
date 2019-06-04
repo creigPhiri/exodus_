68e6c6829b7a722e2b20a7c6f4e3b3fbc21a6ba1
Creig Fortune <fortunephiri004@gmail.com>
2019-06-04 12:03:08 -0400

    update to login screen

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,19 +1,19 @@
 @include('layouts.layout')
 
-<div class="h-100" style="background-image: url('/images/authBackground.jpg')">
-    <div class="row justify-content-center align-self-center">
-        <div class="col-md-6 col-lg-4 py-5 ">
-            <div class="card">
-                <div class="card-header h4">{{ __('Login') }}</div>
+<div class="h-100 flex-wrap" style="background-color: #0B0C10">
+    <div class="pt-5 row justify-content-center ">
+        <div class="col-md-10 col-lg-4 col-10 py-5 ">
+            <div class="mt-4 card p-4" style="background-color: #1F2833; border-radius: 20px">
+                <div class="card-header h4" style="color: #FFFFFA ">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="text-white col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
@@ -25,9 +25,9 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right text-white">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -39,21 +39,21 @@
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-8 offset-md-2">
                                 <div class="checkbox">
-                                    <label>
+                                    <label class="text-white">
                                         <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
                                     </label>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary col-10 offset-1">
+                        <div class="form-group row mb-0 mx-auto">
+                            <div class="col-md-8 offset-md-2">
+                                <button type="submit" class="btn col-12 primary_color" style=" color: #FFFFFA">
                                     {{ __('Login') }}
                                 </button>
-                                <a class="btn btn-link" href="{{ route('register') }}">New User ?</a>
+                                <a class="btn btn-link" style="color: #FB8122" href="{{ route('register') }}">New User ?</a>
                                 {{--<a class="btn btn-link" href="{{ route('password.request') }}">--}}
                                     {{--{{ __('Forgot Your Password?') }}--}}
                                 {{--</a>--}}
