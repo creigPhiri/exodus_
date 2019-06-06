275cda0c2fc9b41c3c830c75fdf03ef15e6333bd
Creig Fortune <fortunephiri004@gmail.com>
2019-06-06 01:47:25 -0400

    adding grey scale background to form

--- resources/views/test_folder/trending.blade.php
+++ resources/views/test_folder/trending.blade.php
@@ -8,12 +8,12 @@
 
     <h2 class="text-center h2 off_white" style="font-family: 'Indie Flower',cursive; ">Trending now</h2>
     <div class=" d-flex justify-content-center row">
-        <form method="post" action="./makePost" class="form mr-auto col-6 offset-3 pt-2">
+        <form method="post" action="/makePost" class="form bg-light  mr-auto col-6 offset-3 pt-2">
             {{csrf_field()}}
             <div class="form-group row">
                 <div class="col-8">
-                    <input type="text" name="post_title" class=" p-2 mb-1 col rounded " placeholder="give your article a title">
-                    <input name="post_body" type="text" class="p-2 col rounded " placeholder="share your thoughts {{Auth::user()->name}} ..."/>
+                    <input type="text" name="post_title" class=" form-control p-2 mb-1 col rounded " placeholder="give your article a title">
+                    <input name="post_body" type="text" class=" form-control p-2 col rounded " placeholder="share your thoughts {{Auth::user()->name}} ..."/>
                 </div>
                 <div class="col py-4">
                     <input type="submit" value="Submit" class="col p-2 rounded primary_color_bg text-white">
