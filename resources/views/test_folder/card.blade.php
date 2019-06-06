967e22db80f458deb39fa2fbe1d7d2d187af30ef
Creig Fortune <fortunephiri004@gmail.com>
2019-06-06 02:00:54 -0400

    removing temp image

--- resources/views/test_folder/card.blade.php
+++ resources/views/test_folder/card.blade.php
@@ -6,14 +6,14 @@
             <div class="card-header" style="background-color: white; border-bottom: none">
                 <h3 class="h3 font-weight-bold" style="font-family: 'Quattrocento Sans', sans-serif; ">{{$post->post_title}}</h3>
                 <div class="row">
-                    <div class=" col-3 col-sm-3 justify-content-center align-items-center">
-                        <img src="https://http2.mlstatic.com/quadro-personalidade-nelson-mande-29x42-moldura-madeira-1102-D_NQ_NP_980480-MLB28001804510_082018-F.jpg" alt="" width="60px" height="60px" class="mb-2 rounded-circle img-responsive">
-                        <span class="d-sm-block primary_color d-inline ">{{'@'.$post->users->name}}</span>
+                    <div class=" col-3 col-sm-3 my-auto">
+                        <div style="width: 60px; height:60px; font-size: 30px; border-radius: 50%" class=" col mx-auto bg-dark text-white text-capitalize justify-content-center text-center">{{substr($post->users->name,0,1)}}</div>
+                        <span class="d-sm-block text-center primary_color d-inline ">{{'@'.$post->users->name}}</span>
                     </div>
-                    <div class=" col row">
+                    <div class=" col row justify-content-center">
                         <div class="col-12" >
                             <div class="" style ="max-height: 100px;overflow-y: scroll" >
-                                <p class=""  style="font-size: 17px; font-family: 'Quattrocento Sans'">{{ $post->post_body }} </p>
+                                <p class=""  style="font-size: 18px; font-family: 'Quattrocento Sans'">{{ $post->post_body }} </p>
                             </div>
                             <div class="">
                                 <span class="text-muted pl-3 mr-1" style="font-size: 12px">{{$post->created_at->diffforhumans()}}</span>
