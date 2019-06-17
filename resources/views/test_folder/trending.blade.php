f9bb2031925a51029a60599655a176f968c1e155
Creig Fortune <fortunephiri004@gmail.com>
2019-06-17 05:02:01 -0400

    update to trending.blade. removing commenting form for collapsible bootstrap div

--- resources/views/test_folder/trending.blade.php
+++ resources/views/test_folder/trending.blade.php
@@ -5,10 +5,11 @@
 <body class="background_color">
 @include('layouts.nav')
 <div class="container-fluid" style=";height: 100%">
-
-    <h2 class="text-center h2 off_white" style="font-family: 'Indie Flower',cursive; ">Trending now</h2>
-    <div class=" d-flex justify-content-center row">
-        <form method="post" action="/makePost" class="form rounded mr-auto col-6 offset-3 p-4" style="background-color: #1B1F3C">
+    <div class="text-center mb-5">
+        <a data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample" class="text-center h2 collapsing text-white " style="font-family: 'Indie Flower',cursive; ">Add your views <i class="fas fa-plus primary_color"></i></a>
+    </div>
+    <div id="collapseExample" class=" collapse mt-0 jumbotron bg-transparent justify-content-center row p-5">
+        <form method="post" action="/makePost" class="rounded mr-auto col-6 offset-3">
             {{csrf_field()}}
             <div class="form-group row ">
                 <div class="col-8">
